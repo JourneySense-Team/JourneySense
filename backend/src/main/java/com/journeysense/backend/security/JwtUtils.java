cc80ff7d04afcfc95f74747618c2a2ffbe4aea04
lucianpopescu688 <lucianpopescu688@gmail.com>
2025-11-22 19:36:23 +0200

    feat: good GUI

--- backend/src/main/java/com/journeysense/backend/security/JwtUtils.java
+++ backend/src/main/java/com/journeysense/backend/security/JwtUtils.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 public class JwtUtils {
 
     // In production, move this to application.properties
-    @Value("${application.security.jwt.secret-key:404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970}")
+    @Value("${application.security.jwt.secret-key}")
     private String secretKey;
 
     @Value("${application.security.jwt.expiration:86400000}") // 1 day
